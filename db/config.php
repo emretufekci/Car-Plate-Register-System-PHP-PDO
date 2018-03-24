91ba1cd590a87d677dc0fef570f2ef6fb89e25fd
Ercan Havare <ercanhavare@gmail.com>
2018-03-24 10:59:09 +0200

    configuration
    configuration of database

---
+++ db/config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ercan Havare
+ * Date: 3/24/2018
+ * Time: 10:28 AM
+ */
+
+define("DB_HOST","localhost");
+define("DB_USER","root");
+define("DB_PASS","");
+define("DB_NAME","car_plate_program");
